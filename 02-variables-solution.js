8cdf21650eda7a618c289ec1470534b363f9a133
noufanelachola <noufansadiqelachola@gmail.com>
2023-07-23 07:52:28 +0530

    02-variables

---
+++ 02-variables-solution.js
@@ -0,0 +1,9 @@
+// create two variables , firstName and lastName
+// and store your name
+let firstName = "Noufan";
+let lastName = "Elachola";
+
+// And then output yourname in the console
+console.log("My name is " + firstName + " " + lastName);
+
+
