64f2d882a920b7f70e03b63e3d1867af446e547d
noufanelachola <noufansadiqelachola@gmail.com>
2023-07-26 11:57:42 +0530

    03-types

---
+++ 03-Types-solution.js
@@ -0,0 +1,20 @@
+// Declare two variables, basePower and bonusPower, and assign them any magical numbers of your choice. 
+// Then, calculate the total spell power by adding these two numbers together and display the result using 
+// console.log().
+
+let basePower = 80;
+let bonusPower = 20;
+let totalSpellPower = basePower + bonusPower;
+console.log("Total Spell Power: " + totalSpellPower);
+
+
+
+// Declare three variables, spell1, spell2, and spell3, and assign them magical words or incantations. Then, 
+// concatenate the three spells together with a space in between and display the resulting magical incantation 
+// using alert().
+
+let spell1 = "Abracadabra";
+let spell2 = "Wingardium";
+let spell3 = "Leviosa";
+let magicalIncantation = spell1 + " " + spell2 + " " + spell3;
+alert("Magical Incantation: " + magicalIncantation);
---
+++ 03-Types.js
@@ -0,0 +1,17 @@
+// Declare two variables, basePower and bonusPower, and assign them any magical numbers of your choice. 
+// Then, calculate the total spell power by adding these two numbers together and display the result using 
+// console.log().
+
+
+
+
+
+
+
+
+
+
+
+// Declare three variables, spell1, spell2, and spell3, and assign them magical words or incantations. Then, 
+// concatenate the three spells together with a space in between and display the resulting magical incantation 
+// using alert().
