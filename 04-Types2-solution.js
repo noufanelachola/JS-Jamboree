7e647e5f6249ce948c5a834726a14757944c73e7
noufanelachola <noufansadiqelachola@gmail.com>
2023-07-26 18:34:05 +0530

    04-types2

---
+++ 04-Types2-solution.js
@@ -0,0 +1,12 @@
+// Choose the right answers from the given options
+
+let a; // 1. What is the initial value of variable 'a'? 
+// Answer: The initial value of 'a' is 'undefined'.
+
+console.log(a); // 2. What will this print? (a) undefined (b) null (c) Error (d) 0
+// Answer: (a) undefined
+
+let b = 5; 
+let c; 
+console.log(b + c); // 3. What will this print? (a) undefined (b) null (c) NaN (d) 5undefined
+// Answer: (c) NaN (Not a Number)
---
+++ 04-Types2.js
@@ -0,0 +1,10 @@
+// Choose the right answers from the given options
+
+let a; // 1. What is the initial value of variable 'a'? 
+
+console.log(a); // 2. What will this print? (a) undefined (b) null (c) Error (d) 0
+
+
+let b = 5; 
+let c; 
+console.log(b + c); // 3. What will this print? (a) undefined (b) null (c) NaN (d) 5undefined
