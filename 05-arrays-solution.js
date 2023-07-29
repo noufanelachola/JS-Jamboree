eb7937bc4f7ac10363fc52e0306c10c7fca866f3
noufanelachola <noufansadiqelachola@gmail.com>
2023-07-29 11:56:19 +0530

    05-arrays

---
+++ 05-arrays-solution.js
@@ -0,0 +1,14 @@
+// Declare an array called fruits and initialize it with the following fruits: "apple", "banana", "orange", "grape", "mango".
+let fruits = ["apple", "banana", "orange", "grape", "mango"];
+
+
+// Log the third fruit from the fruits array to the console.
+console.log(fruits[2]);
+
+
+// Add "strawberry" to the end of the fruits array.
+fruits.push("strawberry");
+
+
+// Remove the first fruit from the fruits array.
+fruits.shift();
