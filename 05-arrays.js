6520db1737a61530a09802db5c36d0ab061028f1
noufanelachola <noufansadiqelachola@gmail.com>
2023-07-29 11:55:59 +0530

    05-arrays

---
+++ 05-arrays.js
@@ -0,0 +1,13 @@
+// Declare an array called fruits and initialize it with the following fruits: "apple", "banana", "orange", "grape", "mango".
+
+
+
+// Log the third fruit from the fruits array to the console.
+
+
+
+// Add "strawberry" to the end of the fruits array.
+
+
+
+// Remove the first fruit from the fruits array.
