a7917ebb50803d34333425e62f1a2a4a425e377e
noufanelachola <noufansadiqelachola@gmail.com>
2023-07-31 23:18:34 +0530

    06-obj

---
+++ 06-objects-solution.js
@@ -0,0 +1,21 @@
+// Create a student1IDCard object representing a student with the following properties:
+// Name: "Alice Johnson"
+// Age: 21
+// Major: "Biology"
+
+const student1IDCard = {
+    name: "Alice Johnson",
+    age: 21,
+    major: "Biology",
+};
+
+
+
+// Add a property "GPA" with the value 3.9 to student1IDCard.
+
+student1IDCard.GPA = 3.9;
+
+
+// Add a property "courses" as an array containing "Chemistry" and "Anatomy" to student1IDCard.
+
+student1IDCard.courses = ["Chemistry", "Anatomy"];
---
+++ 06-objects.js
@@ -0,0 +1,15 @@
+// Create a student1IDCard object representing a student with the following properties:
+// Name: "Alice Johnson"
+// Age: 21
+// Major: "Biology"
+
+
+
+// Add a property "GPA" with the value 3.9 to student1IDCard.
+
+
+
+
+
+
+// Add a property "courses" as an array containing "Chemistry" and "Anatomy" to student1IDCard.s
