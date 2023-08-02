b1b914abc81b09f256e61490dc478d81350c016f
noufanelachola <noufansadiqelachola@gmail.com>
2023-08-03 04:49:43 +0530

    if and else

---
+++ 07-if-else.js
@@ -0,0 +1,12 @@
+// Read the given weather condition (e.g., "sunny", "rainy", or any other weather) in the variable.
+// Using conditional statements (if, else if, and else), determine the appropriate outfit to wear based on the weather condition:
+// If the weather is "sunny", set the outfit variable to "T-shirt and shorts".
+// If the weather is "rainy", set the outfit variable to "Raincoat and boots".
+// For any other weather condition, set the outfit variable to "Jacket and jeans".
+// After setting the outfit, the code will print a message indicating today's weather and the recommended outfit.
+
+const weather = "sunny"; 
+let outfit;
+
+
+console.log(`Today's weather is ${weather}. Wear: ${outfit}.`);
