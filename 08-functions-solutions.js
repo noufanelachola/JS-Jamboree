2f6cacca8c92fc5605d0c69318b12379a18edaa5
noufanelachola <noufansadiqelachola@gmail.com>
2023-08-04 07:57:29 +0530

    functions

---
+++ 08-functions-solutions.js
@@ -0,0 +1,24 @@
+// Create three superhero greeting functions using the provided magic formula:
+
+// greetBatman(): This function should greet Batman with "I'm Batman!"
+function greetBatman() {
+    console.log("I'm Batman!");
+}
+
+
+// greetSuperman(): This function should greet Superman with "Up, up, and away!"
+function greetSuperman() {
+    console.log("Up, up, and away!");
+}
+
+
+// greetWonderWoman(): This function should greet Wonder Woman with "Greetings, Amazon Princess!"
+function greetWonderWoman() {
+    console.log("Greetings, Amazon Princess!");
+}
+
+
+// Invoke each function to send powerful greetings to the superheroes!
+greetBatman();      // Output: "I'm Batman!"
+greetSuperman();    // Output: "Up, up, and away!"
+greetWonderWoman(); // Output: "Greetings, Amazon Princess!"
---
+++ 08-functions.js
@@ -0,0 +1,9 @@
+// Create three superhero greeting functions using the provided magic formula:
+
+// greetBatman(): This function should greet Batman with "I'm Batman!"
+
+// greetSuperman(): This function should greet Superman with "Up, up, and away!"
+
+// greetWonderWoman(): This function should greet Wonder Woman with "Greetings, Amazon Princess!"
+
+// Invoke each function to send powerful greetings to the superheroes!
